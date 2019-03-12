d55c3e053d74bb20cfb34bf3d4392021662d5ed5
EvannyR <evanny.rira@outlook.com>
2019-03-12 17:04:09 -0600

    pipe integrado

---
+++ src/pages/costumer/costumer.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { AboutPage } from '../about/about';
+
+/**
+ * Generated class for the CostumerPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-costumer',
+  templateUrl: 'costumer.html',
+})
+export class CostumerPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CostumerPage');
+  }
+  back(){
+    this.navCtrl.pop();
+  }
+  logOut(){
+    this.navCtrl.popToRoot();
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  user:string;
+  group:string;
+  cursos:string[]=['ionic','angulas','docker','liquibase'];
+  money: number;
+  constructor(public navCtrl: NavController, navParams: NavParams) {
   }
 
+  goAbout(){
+    let data = {user:this.user,group:this.group,courses:this.cursos,date: new Date(),money:this.money}
+    this.navCtrl.push(AboutPage,data);
+  }
 }
